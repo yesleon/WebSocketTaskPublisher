590b3b7a6610b28369684a952d581a9d3c695ee7
Li-Heng Hsu <yesleon@me.com>
2021-03-02 19:13:47 +0800

    rename to WebSocketTaskPublisher

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "WebSocketPublisher",
+    name: "WebSocketTaskPublisher",
     platforms: [
         .iOS(.v13),
         .macOS(.v10_15)
@@ -12,8 +12,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "WebSocketPublisher",
-            targets: ["WebSocketPublisher"]),
+            name: "WebSocketTaskPublisher",
+            targets: ["WebSocketTaskPublisher"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,10 +23,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "WebSocketPublisher",
+            name: "WebSocketTaskPublisher",
             dependencies: []),
         .testTarget(
-            name: "WebSocketPublisherTests",
-            dependencies: ["WebSocketPublisher"]),
+            name: "WebSocketTaskPublisherTests",
+            dependencies: ["WebSocketTaskPublisher"]),
     ]
 )
---
+++ Sources/WebSocketTaskPublisher/WebSocketTaskPublisher.swift
@@ -0,0 +1,87 @@
+//
+//  WebSocketTaskPublisher.swift
+//  WebSocketTaskPublisher
+//
+//  Created by Li-Heng Hsu on 2021/2/28.
+//
+
+import Combine
+import Foundation
+
+extension URLSession {
+    
+    public func webSocketTaskPublisher(
+        for url: URL,
+        taskConfigurationHandler: @escaping (WebSocketTask) -> Void
+    ) -> WebSocketTaskPublisher {
+        
+        return WebSocketTaskPublisher(session: self, request: .init(url: url), taskConfigurationHandler: taskConfigurationHandler)
+    }
+    
+    public func webSocketTaskPublisher(
+        for request: URLRequest, taskConfigurationHandler: @escaping (WebSocketTask) -> Void) -> WebSocketTaskPublisher {
+        
+        .init(session: self, request: request, taskConfigurationHandler: taskConfigurationHandler)
+    }
+    
+    public struct WebSocketTaskPublisher: Publisher {
+        public typealias Output = URLSessionWebSocketTask.Message
+        public typealias Failure = Error
+        
+        private class Subscription<S: Subscriber>: Combine.Subscription where S.Failure == Error, S.Input == URLSessionWebSocketTask.Message {
+            
+            var task: URLSessionWebSocketTask?
+            let subscriber: S
+            
+            init(task: URLSessionWebSocketTask, subscriber: S) {
+                self.task = task
+                self.subscriber = subscriber
+            }
+            
+            func request(_ demand: Subscribers.Demand) {
+                
+                if let limit = demand.max {
+                    guard limit > 0 else { return }
+                }
+                task?.receive { [weak self] result in
+                    guard let self = self else { return }
+                    switch result {
+                    case .success(let message):
+                        let additionalDemand = self.subscriber.receive(message)
+                        if let demand = demand.max, let additionalDemand = additionalDemand.max {
+                            self.request(.max(demand + additionalDemand))
+                        } else {
+                            self.request(.unlimited)
+                        }
+                    case .failure(let error):
+                        self.subscriber.receive(completion: .failure(error))
+                        self.cancel()
+                    }
+                }
+            }
+            
+            func cancel() {
+                task?.cancel(with: .normalClosure, reason: nil)
+                task = nil
+            }
+        }
+        
+        let session: URLSession
+        let request: URLRequest
+        let taskConfigurationHandler: (WebSocketTask) -> Void
+        
+        init(session: URLSession, request: URLRequest, taskConfigurationHandler: @escaping (WebSocketTask) -> Void) {
+            self.session = session
+            self.request = request
+            self.taskConfigurationHandler = taskConfigurationHandler
+        }
+        
+        public func receive<S>(subscriber: S) where S : Subscriber, Self.Failure == S.Failure, Self.Output == S.Input {
+            let task = session.webSocketTask(with: request)
+            task.resume()
+            taskConfigurationHandler(task)
+            let subscription = Subscription(task: task, subscriber: subscriber)
+            subscriber.receive(subscription: subscription)
+        }
+    }
+}
