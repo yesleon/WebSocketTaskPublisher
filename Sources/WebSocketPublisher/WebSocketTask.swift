8aa404f57a4c80b20243378f4c5b226ccbb14e4a
Li-Heng Hsu <yesleon@me.com>
2021-03-02 13:29:27 +0800

    first commit

---
+++ Sources/WebSocketPublisher/WebSocketTask.swift
@@ -0,0 +1,36 @@
+//
+//  File.swift
+//  
+//
+//  Created by 許立衡 on 2021/3/2.
+//
+
+import Foundation
+
+
+public protocol WebSocketTask {
+    func send(_ message: URLSessionWebSocketTask.Message, completion: @escaping () -> Void)
+    func ping(completion: @escaping () -> Void)
+}
+
+extension WebSocketTask {
+    
+    func send(_ message: URLSessionWebSocketTask.Message) {
+        send(message) { }
+    }
+    
+    func ping() {
+        ping { }
+    }
+}
+
+extension URLSessionWebSocketTask: WebSocketTask {
+    
+    public func send(_ message: Message, completion: @escaping () -> Void) {
+        send(message, completionHandler: { _ in completion() })
+    }
+    
+    public func ping(completion: @escaping () -> Void) {
+        sendPing(pongReceiveHandler: { _ in completion() })
+    }
+}
